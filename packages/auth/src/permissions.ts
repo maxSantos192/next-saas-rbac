13f94c2fcf07c75a4736bd8d1265593cae821ac2
Maxson Santos <maxsonsantos88@gmail.com>
2025-10-07 00:06:54 -0300

    feat: add initial permissions

---
+++ packages/auth/src/permissions.ts
@@ -0,0 +1,20 @@
+import { AbilityBuilder } from '@casl/ability';
+
+import { AppAbility } from '.';
+import { User } from './models/user';
+
+type Role = 'ADMIN' | 'MEMBER';
+
+type PermissionByRole = (
+  user: User,
+  builder: AbilityBuilder<AppAbility>
+) => void;
+
+export const permissions: Record<Role, PermissionByRole> = {
+  ADMIN(_, { can }) {
+    can('manage', 'all');
+  },
+  MEMBER(_, { can }) {
+    can('invite', 'User');
+  },
+};
