a2895676a34decb7240b294a34b4b03e42a6e432
Maxson Santos <maxsonsantos88@gmail.com>
2025-11-17 19:11:15 -0300

    feat: implement custom error handling

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -2,7 +2,7 @@ import fastifyCors from '@fastify/cors';
 import fastifyJwt from '@fastify/jwt';
 import fastifySwagger from '@fastify/swagger';
 import fastifySwaggerUI from '@fastify/swagger-ui';
-import { fastify } from 'fastify';
+import fastify from 'fastify';
 import {
   jsonSchemaTransform,
   serializerCompiler,
@@ -10,14 +10,15 @@ import {
   ZodTypeProvider,
 } from 'fastify-type-provider-zod';
 
+import { errorHandler } from './error-handler';
 import { routes } from './routes';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-app.register(fastifyCors);
+app.setErrorHandler(errorHandler);
 
 app.register(fastifySwagger, {
   openapi: {
@@ -46,6 +47,8 @@ app.register(fastifySwaggerUI, {
 app.register(fastifyJwt, {
   secret: 'superSecret',
 });
+
+app.register(fastifyCors);
 
 app.register(routes);
 
