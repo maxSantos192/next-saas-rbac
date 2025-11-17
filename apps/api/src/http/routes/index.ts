addfcc84ffdf1334dce180671b89bf72dec0e54f
Maxson Santos <maxsonsantos88@gmail.com>
2025-11-17 20:58:23 -0300

    feat: add password recovery and reset routes

--- apps/api/src/http/routes/index.ts
+++ apps/api/src/http/routes/index.ts
@@ -3,9 +3,13 @@ import { FastifyInstance } from 'fastify';
 import { authenticateWithPassword } from './auth/authenticate-with-password';
 import { createAccount } from './auth/create-account';
 import { getProfile } from './auth/get-profile';
+import { requestPasswordRecover } from './auth/request-password-recover';
+import { resetPassword } from './auth/reset-password';
 
 export async function routes(app: FastifyInstance) {
-  app.register(createAccount);
   app.register(authenticateWithPassword);
+  app.register(createAccount);
   app.register(getProfile);
+  app.register(requestPasswordRecover);
+  app.register(resetPassword);
 }
