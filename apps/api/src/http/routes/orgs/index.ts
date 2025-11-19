bafeb133ed813bc895fe91a5fbd4c498b59e3d89
Maxson Santos <maxsonsantos88@gmail.com>
2025-11-19 19:45:26 -0300

    fix: fix name of file route

--- apps/api/src/http/routes/orgs/index.ts
+++ apps/api/src/http/routes/orgs/index.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from 'fastify';
 
-import { createOrganization } from './create-organizarion';
+import { createOrganization } from './create-organization';
 
 export async function orgsRoutes(app: FastifyInstance) {
   app.register(createOrganization);
