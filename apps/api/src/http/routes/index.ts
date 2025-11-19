95a8e99b10ff14c2aad1101d6af06dd3f4a1d28d
Maxson Santos <maxsonsantos88@gmail.com>
2025-11-18 23:50:57 -0300

    feat: add GitHub authentication route

--- apps/api/src/http/routes/index.ts
+++ apps/api/src/http/routes/index.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from 'fastify';
 
+import { authenticateWithGithub } from './auth/authenticate-with-github';
 import { authenticateWithPassword } from './auth/authenticate-with-password';
 import { createAccount } from './auth/create-account';
 import { getProfile } from './auth/get-profile';
@@ -7,6 +8,7 @@ import { requestPasswordRecover } from './auth/request-password-recover';
 import { resetPassword } from './auth/reset-password';
 
 export async function routes(app: FastifyInstance) {
+  app.register(authenticateWithGithub);
   app.register(authenticateWithPassword);
   app.register(createAccount);
   app.register(getProfile);
