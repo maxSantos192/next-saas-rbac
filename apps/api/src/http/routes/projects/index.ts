23bdff91ca051c01ec53bbe36bc074497250a626
Maxson Santos <maxsonsantos88@gmail.com>
2025-11-25 18:23:53 -0300

    feat: add get project route

--- apps/api/src/http/routes/projects/index.ts
+++ apps/api/src/http/routes/projects/index.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify';
 
 import { createProject } from './create-project';
+import { getProject } from './get-project';
 
 export async function projectsRoutes(app: FastifyInstance) {
   app.register(createProject);
+  app.register(getProject);
 }
