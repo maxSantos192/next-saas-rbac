79b024edc1b31a5ddf4f7f774b086e8c0ca873ac
Maxson Santos <maxsonsantos88@gmail.com>
2025-11-25 15:51:55 -0300

    feat: add shutdown and update organization routes

--- apps/api/src/http/routes/orgs/index.ts
+++ apps/api/src/http/routes/orgs/index.ts
@@ -4,10 +4,14 @@ import { createOrganization } from './create-organization';
 import { getMembership } from './get-membership';
 import { getOrganization } from './get-organization';
 import { getOrganizations } from './get-organizations';
+import { shutdownOrganization } from './shutdown-organization';
+import { updateOrganization } from './update-organization';
 
 export async function orgsRoutes(app: FastifyInstance) {
   app.register(createOrganization);
   app.register(getMembership);
   app.register(getOrganization);
   app.register(getOrganizations);
+  app.register(shutdownOrganization);
+  app.register(updateOrganization);
 }
