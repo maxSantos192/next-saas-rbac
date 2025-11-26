38ae2b2c57809e68f8058dfae4e90c560bea9d0f
Maxson Santos <maxsonsantos88@gmail.com>
2025-11-25 22:25:31 -0300

    feat: add delete project route

--- apps/api/src/http/routes/projects/index.ts
+++ apps/api/src/http/routes/projects/index.ts
@@ -1,12 +1,14 @@
 import { FastifyInstance } from 'fastify';
 
 import { createProject } from './create-project';
+import { deleteProject } from './delete-project';
 import { getProject } from './get-project';
 import { getProjects } from './get-projects';
 import { updateProject } from './update-project';
 
 export async function projectsRoutes(app: FastifyInstance) {
   app.register(createProject);
+  app.register(deleteProject);
   app.register(getProject);
   app.register(getProjects);
   app.register(updateProject);
