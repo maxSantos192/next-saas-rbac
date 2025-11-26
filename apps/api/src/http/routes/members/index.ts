70bb089954d3179d465eeb7bdf2b670ec2e87130
Maxson Santos <maxsonsantos88@gmail.com>
2025-11-25 23:26:17 -0300

    feat: add update member route

--- apps/api/src/http/routes/members/index.ts
+++ apps/api/src/http/routes/members/index.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify';
 
 import { getMembers } from './get-members';
+import { updateMember } from './update-member';
 
 export async function membersRoutes(app: FastifyInstance) {
   app.register(getMembers);
+  app.register(updateMember);
 }
