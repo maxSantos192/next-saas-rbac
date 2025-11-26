0f49fc8df757082ac882a9bf950ef8ba8ef731c7
Maxson Santos <maxsonsantos88@gmail.com>
2025-11-26 09:51:37 -0300

    feat: add accept invite route

--- apps/api/src/http/routes/invites/index.ts
+++ apps/api/src/http/routes/invites/index.ts
@@ -1,10 +1,12 @@
 import { FastifyInstance } from 'fastify';
 
+import { acceptInvite } from './accept-invite';
 import { createInvite } from './create-invite';
 import { getInvite } from './get-invite';
 import { getInvites } from './get-invites';
 
 export async function invitesRoutes(app: FastifyInstance) {
+  app.register(acceptInvite);
   app.register(createInvite);
   app.register(getInvite);
   app.register(getInvites);
