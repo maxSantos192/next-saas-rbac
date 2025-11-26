571d89e583c4f08f6f1652e33873679f8d2c7187
Maxson Santos <maxsonsantos88@gmail.com>
2025-11-25 23:26:50 -0300

    feat: add remove member route

--- apps/api/src/http/routes/members/index.ts
+++ apps/api/src/http/routes/members/index.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from 'fastify';
 
 import { getMembers } from './get-members';
+import { removeMember } from './remove-member';
 import { updateMember } from './update-member';
 
 export async function membersRoutes(app: FastifyInstance) {
   app.register(getMembers);
+  app.register(removeMember);
   app.register(updateMember);
 }
