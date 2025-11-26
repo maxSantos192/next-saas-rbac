4bce5f7704dab77d9824e8889593e05fce1378c5
Maxson Santos <maxsonsantos88@gmail.com>
2025-11-26 00:43:53 -0300

    feat: add get all organization invites route

--- apps/api/src/http/routes/invites/index.ts
+++ apps/api/src/http/routes/invites/index.ts
@@ -2,8 +2,10 @@ import { FastifyInstance } from 'fastify';
 
 import { createInvite } from './create-invite';
 import { getInvite } from './get-invite';
+import { getInvites } from './get-invites';
 
 export async function invitesRoutes(app: FastifyInstance) {
   app.register(createInvite);
   app.register(getInvite);
+  app.register(getInvites);
 }
