46ce6b7138bf5db9705cb3cc15c364eb07217917
Maxson Santos <maxsonsantos88@gmail.com>
2025-11-26 09:55:40 -0300

    feat: add reject invite route

--- apps/api/src/http/routes/invites/index.ts
+++ apps/api/src/http/routes/invites/index.ts
@@ -4,10 +4,12 @@ import { acceptInvite } from './accept-invite';
 import { createInvite } from './create-invite';
 import { getInvite } from './get-invite';
 import { getInvites } from './get-invites';
+import { rejectInvite } from './reject-invite';
 
 export async function invitesRoutes(app: FastifyInstance) {
   app.register(acceptInvite);
   app.register(createInvite);
   app.register(getInvite);
   app.register(getInvites);
+  app.register(rejectInvite);
 }
---
+++ apps/api/src/http/routes/invites/reject-invite.ts
@@ -0,0 +1,67 @@
+import { FastifyInstance } from 'fastify';
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { z } from 'zod';
+
+import { auth } from '@/http/middlewares/auth';
+import { prisma } from '@/lib/prisma';
+
+import { BadRequestError } from '../_errors/bad-request-error';
+import { UnauthorizedError } from '../_errors/unauthorized-error';
+
+export async function rejectInvite(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/invites/:inviteId/reject',
+      {
+        schema: {
+          tags: ['Invites'],
+          summary: 'Reject an invite',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            inviteId: z.uuid(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { inviteId } = request.params;
+        const userId = await request.getCurrentUserId();
+
+        const invite = await prisma.invite.findUnique({
+          where: {
+            id: inviteId,
+          },
+        });
+
+        if (!invite) {
+          throw new BadRequestError('invite not found or expired.');
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            id: userId,
+          },
+        });
+
+        if (!user) {
+          throw new BadRequestError('user not found.');
+        }
+
+        if (invite.email !== user.email) {
+          throw new UnauthorizedError('this invites belongs to another user.');
+        }
+
+        await prisma.invite.delete({
+          where: {
+            id: inviteId,
+          },
+        });
+
+        return reply.status(204).send();
+      }
+    );
+}
