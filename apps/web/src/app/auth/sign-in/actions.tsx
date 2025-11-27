7c397c7c76f93c1356d5f1c546f7ddf20ae767d9
Maxson Santos <maxsonsantos88@gmail.com>
2025-11-27 14:26:02 -0300

    feat: create sign in page with funcionality

---
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -0,0 +1,44 @@
+'use server';
+
+import { HTTPError } from 'ky';
+import { z } from 'zod';
+
+import { signInWithPassword } from '@/http/sign-in-with-password';
+
+const signInSchema = z.object({
+  email: z.email({ message: 'Provide a valid e-mail address.' }),
+  password: z.string().min(1, { message: 'Provide your password.' }),
+});
+
+export async function signInWithEmailAndPassword(data: FormData) {
+  const result = signInSchema.safeParse(Object.fromEntries(data));
+
+  if (!result.success) {
+    const errors = result.error.flatten((issue) => issue.message).fieldErrors;
+
+    return { success: false, message: null, errors };
+  }
+
+  const { email, password } = result.data;
+
+  try {
+    const { token } = await signInWithPassword({
+      email,
+      password,
+    });
+
+    console.log(token);
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json();
+
+      return { success: false, message, errors: null };
+    }
+
+    console.error(err);
+
+    return { success: false, message: 'unexpected error.', errors: null };
+  }
+
+  return { success: true, message: null, errors: null };
+}
---
+++ apps/web/src/app/auth/sign-in/sign-in-form.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import { AlertTriangle, Loader2 } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import githubIcon from '@/assets/github-icon.svg';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Separator } from '@/components/ui/separator';
+import { useFormState } from '@/hooks/use-form-state';
+
+import { signInWithEmailAndPassword } from './actions';
+
+export function SignInForm() {
+  const [{ success, message, errors }, handleSubmit, isPending] = useFormState(
+    signInWithEmailAndPassword
+  );
+
+  return (
+    <form onSubmit={handleSubmit} className="space-y-6">
+      <h1>
+        {success === false && message && (
+          <Alert variant="destructive">
+            <AlertTriangle className="size-4" />
+            <AlertTitle>Sign In failed!</AlertTitle>
+            <AlertDescription>
+              <p>{message}</p>
+            </AlertDescription>
+          </Alert>
+        )}
+      </h1>
+
+      <div className="space-y-1">
+        <Label htmlFor="email">E-mail</Label>
+        <Input name="email" type="email" id="email" />
+
+        {errors?.email && (
+          <p className="text-xs font-medium text-red-500 dark:text-red-400">
+            {errors.email[0]}
+          </p>
+        )}
+      </div>
+
+      <div className="space-y-1">
+        <Label htmlFor="password">Password</Label>
+        <Input name="password" type="password" id="password" />
+
+        {errors?.password && (
+          <p className="text-xs font-medium text-red-500 dark:text-red-400">
+            {errors.password[0]}
+          </p>
+        )}
+
+        <Link
+          href="/auth/forgot-password"
+          className="text-foreground text-xs font-medium hover:underline"
+        >
+          Forgot your password?
+        </Link>
+      </div>
+
+      <Button type="submit" className="w-full">
+        {isPending ? <Loader2 className="animate-spin" /> : 'Sign In'}
+      </Button>
+
+      <Separator />
+
+      <Button variant="outline" className="w-full">
+        <Image src={githubIcon} alt="" className="mr-1 size-4 dark:invert" />
+        Sign In with GitHub
+      </Button>
+    </form>
+  );
+}
