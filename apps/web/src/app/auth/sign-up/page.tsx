73f7a94e3f14358ea046bc3114e998fa90c0e31c
Maxson Santos <maxsonsantos88@gmail.com>
2025-12-03 17:45:40 -0300

    feat: add sign-up-form with functionality

--- apps/web/src/app/auth/sign-up/page.tsx
+++ apps/web/src/app/auth/sign-up/page.tsx
@@ -1,53 +1,5 @@
-import Image from 'next/image';
-import Link from 'next/link';
-
-import githubIcon from '@/assets/github-icon.svg';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { Separator } from '@/components/ui/separator';
+import { SignUpForm } from './sign-up-form';
 
 export default function SignUpPage() {
-  return (
-    <form action="" className="space-y-4">
-      <div className="space-y-1">
-        <Label htmlFor="name">Name</Label>
-        <Input name="name" id="name" />
-      </div>
-
-      <div className="space-y-1">
-        <Label htmlFor="email">E-mail</Label>
-        <Input name="email" type="email" id="email" />
-      </div>
-
-      <div className="space-y-1">
-        <Label htmlFor="password">Password</Label>
-        <Input name="password" type="password" id="password" />
-      </div>
-
-      <div className="space-y-1">
-        <Label htmlFor="password_confirmation">Confirm Your Password</Label>
-        <Input
-          name="password_confirmation"
-          type="password"
-          id="password_confirmation"
-        />
-      </div>
-
-      <Button type="submit" className="w-full">
-        Create account
-      </Button>
-
-      <Button variant="link" size="sm" className="w-full" asChild>
-        <Link href="/auth/sign-in">Alredy registered? Sign In</Link>
-      </Button>
-
-      <Separator />
-
-      <Button type="submit" variant="outline" className="w-full">
-        <Image src={githubIcon} alt="" className="mr-1 size-4 dark:invert" />
-        Sign Up with GitHub
-      </Button>
-    </form>
-  );
+  return <SignUpForm />;
 }
