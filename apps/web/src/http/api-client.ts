5e9cafc5089b1cf75a082edbae8efdb03aca27c1
Maxson Santos <maxsonsantos88@gmail.com>
2025-12-03 15:42:30 -0300

    feat: add cookie management and get-profile functionality

--- apps/web/src/http/api-client.ts
+++ apps/web/src/http/api-client.ts
@@ -2,4 +2,27 @@ import ky from 'ky';
 
 export const api = ky.create({
   prefixUrl: 'http://localhost:3333',
+  hooks: {
+    beforeRequest: [
+      async (request) => {
+        let token: string | undefined;
+
+        if (typeof window === 'undefined') {
+          const { cookies } = await import('next/headers');
+
+          const cookieStore = await cookies();
+
+          token = cookieStore.get('token')?.value;
+        } else {
+          const { getCookie } = await import('cookies-next');
+
+          token = getCookie('token') as string | undefined;
+        }
+
+        if (token) {
+          request.headers.set('Authorization', `Bearer ${token}`);
+        }
+      },
+    ],
+  },
 });
