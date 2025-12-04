f13437735f03b8ca1dc2c6e3c629c6957d6b839a
Maxson Santos <maxsonsantos88@gmail.com>
2025-12-04 00:53:01 -0300

    feat: add layout to protect routes and update sign-out method to GET

--- apps/web/src/app/api/auth/sign-out/route.ts
+++ apps/web/src/app/api/auth/sign-out/route.ts
@@ -1,7 +1,7 @@
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
-export async function POST() {
+export async function GET() {
   const cookieStore = await cookies();
 
   cookieStore.delete('token');
