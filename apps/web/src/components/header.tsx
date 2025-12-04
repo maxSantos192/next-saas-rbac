949aaab6f8d32f318d813ad1f3a48f37c95e63c6
Maxson Santos <maxsonsantos88@gmail.com>
2025-12-04 18:22:34 -0300

    feat: add organization switcher and middleware for cookie management

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,7 +1,9 @@
+import { Slash } from 'lucide-react';
 import Image from 'next/image';
 
 import nextjsIcon from '@/assets/nextjs-icon.svg';
 
+import { OrganizationSwitcher } from './organization-switcher';
 import { ProfileButton } from './profile-button';
 
 export function Header() {
@@ -9,6 +11,10 @@ export function Header() {
     <header className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
         <Image src={nextjsIcon} alt="NextJs" className="size-6 dark:invert" />
+
+        <Slash className="-rotate-24 text-border size-3" />
+
+        <OrganizationSwitcher />
       </div>
 
       <ProfileButton />
---
+++ apps/web/src/components/organization-switcher.tsx
@@ -0,0 +1,86 @@
+import { ChevronsUpDown, PlusCircle } from 'lucide-react';
+import { cookies } from 'next/headers';
+import Link from 'next/link';
+
+import { getInitials } from '@/helpers/get-initials';
+import { getOrganizations } from '@/http/get-organization';
+
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+
+export async function OrganizationSwitcher() {
+  const cookieStore = await cookies();
+
+  const currentOrg = cookieStore.get('org')?.value;
+  const { organizations } = await getOrganizations();
+
+  const currentOrganization = organizations.find(
+    (org) => org.slug === currentOrg
+  );
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="focus-visible:ring-primary flex w-[168px] cursor-pointer items-center gap-2 rounded p-1 text-sm font-medium outline-none focus-visible:ring-2">
+        {currentOrganization ? (
+          <>
+            <Avatar className="mr-2 size-5">
+              {currentOrganization.avatarUrl && (
+                <AvatarImage src={currentOrganization.avatarUrl} />
+              )}
+              <AvatarFallback>
+                {getInitials(currentOrganization.name)}
+              </AvatarFallback>
+            </Avatar>
+            <span className="truncate">{currentOrganization.name}</span>
+          </>
+        ) : (
+          <span className="text-muted-foreground">Select organization</span>
+        )}
+        <ChevronsUpDown className="text-muted-foreground ml-auto size-4" />
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent
+        align="end"
+        alignOffset={-16}
+        sideOffset={12}
+        className="w-[200px]"
+      >
+        <DropdownMenuGroup>
+          <DropdownMenuLabel>Organizations</DropdownMenuLabel>
+          {organizations.map((organization) => {
+            return (
+              <DropdownMenuItem key={organization.id} asChild>
+                <Link href={`/org/${organization.slug}`}>
+                  <Avatar className="mr-2 size-5">
+                    {organization.avatarUrl && (
+                      <AvatarImage src={organization.avatarUrl} />
+                    )}
+                    <AvatarFallback>
+                      {getInitials(organization.name)}
+                    </AvatarFallback>
+                  </Avatar>
+                  <span className="line-clamp-1">{organization.name}</span>
+                </Link>
+              </DropdownMenuItem>
+            );
+          })}
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href="/create-organization">
+            <PlusCircle className="mr-2 size-5" />
+            Create new
+          </Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
