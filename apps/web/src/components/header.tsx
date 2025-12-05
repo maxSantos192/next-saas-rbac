d8edd21af8b65e8dd2293f8e23f1aca6735f1d20
Maxson Santos <maxsonsantos88@gmail.com>
2025-12-05 17:35:42 -0300

    feat: add @saas/auth for membership management and get-membership fetch route

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -2,11 +2,14 @@ import { Slash } from 'lucide-react';
 import Image from 'next/image';
 
 import nextjsIcon from '@/assets/nextjs-icon.svg';
+import { ability } from '@/auth/auth';
 
 import { OrganizationSwitcher } from './organization-switcher';
 import { ProfileButton } from './profile-button';
 
-export function Header() {
+export async function Header() {
+  const permissions = await ability();
+
   return (
     <header className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
@@ -15,6 +18,8 @@ export function Header() {
         <Slash className="-rotate-24 text-border size-3" />
 
         <OrganizationSwitcher />
+
+        {permissions?.can('get', 'Project') && <p>projetos</p>}
       </div>
 
       <ProfileButton />
