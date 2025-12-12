b6e175d1ec6aa5ea6eb1e54bd9a2af54f5ffc144
Maxson Santos <maxsonsantos88@gmail.com>
2025-12-12 10:23:55 -0300

    feat: add create organization page with creation functionality

---
+++ apps/web/src/app/(app)/org/organization-form.tsx
@@ -0,0 +1,95 @@
+'use client';
+
+import { AlertTriangle, Loader2 } from 'lucide-react';
+
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { useFormState } from '@/hooks/use-form-state';
+
+import { createOrganizationAction } from './action';
+
+export default function OrganizationForm() {
+  const [{ success, message, errors }, handleSubmit, isPending] = useFormState(
+    createOrganizationAction
+  );
+
+  return (
+    <div className="space-y-6">
+      <form onSubmit={handleSubmit} className="space-y-6">
+        {success === false && message && (
+          <Alert variant="destructive">
+            <AlertTriangle className="size-4" />
+            <AlertTitle>Save organization failed!</AlertTitle>
+            <AlertDescription>{message}</AlertDescription>
+          </Alert>
+        )}
+
+        {success === true && message && (
+          <Alert>
+            <AlertTriangle className="size-4" />
+            <AlertTitle>Success!</AlertTitle>
+            <AlertDescription>
+              <p>{message}</p>
+            </AlertDescription>
+          </Alert>
+        )}
+
+        <div className="space-y-1">
+          <Label htmlFor="name">Organization name</Label>
+          <Input name="name" id="name" />
+          {errors?.name && (
+            <p className="text-xs font-medium text-red-500 dark:text-red-400">
+              {errors.name[0]}
+            </p>
+          )}
+        </div>
+
+        <div className="space-y-1">
+          <Label htmlFor="domain">E-mail domain</Label>
+          <Input
+            name="domain"
+            type="text"
+            id="domain"
+            inputMode="url"
+            placeholder="example.com"
+          />
+          {errors?.domain && (
+            <p className="text-xs font-medium text-red-500 dark:text-red-400">
+              {errors.domain[0]}
+            </p>
+          )}
+        </div>
+
+        <div className="space-y-1">
+          <div className="flex items-baseline space-x-2">
+            <Checkbox
+              name="shouldAttachUsersByDomain"
+              id="shouldAttachUsersByDomain"
+              className="translate-y-0.5"
+            />
+            <label htmlFor="shouldAttachUsersByDomain" className="space-y-1">
+              <span className="text-sm font-medium leading-none">
+                Auto-join new members
+              </span>
+              <p className="text-muted-foreground text-sm">
+                This will automatically invite all members with same e-mail
+                domain to this organization.
+              </p>
+            </label>
+          </div>
+        </div>
+
+        <Button type="submit" className="w-full" disabled={isPending}>
+          {isPending ? (
+            <Loader2 className="animate-spin" />
+          ) : (
+            'Save organization'
+          )}
+        </Button>
+      </form>
+    </div>
+  );
+}
