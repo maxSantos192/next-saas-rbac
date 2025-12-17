c34c7aeb932c94596b288095604d1e26a5659f09
Maxson Santos <maxsonsantos88@gmail.com>
2025-12-17 18:36:06 -0300

    feat: add create invite form and functionality

--- apps/web/src/app/(app)/org/[slug]/members/actions.ts
+++ apps/web/src/app/(app)/org/[slug]/members/actions.ts
@@ -1,8 +1,11 @@
 'use server';
 
-import { Role } from '@saas/auth';
+import { Role, roleSchema } from '@saas/auth';
+import { HTTPError } from 'ky';
+import { z } from 'zod';
 
 import { getCurrentOrg } from '@/auth/auth';
+import { createInvite } from '@/http/create-invite';
 import { removeMember } from '@/http/remove-member';
 import { revokeInvite } from '@/http/revoke-invite';
 import { transferOrganization } from '@/http/transfer-organization';
@@ -13,12 +16,51 @@ interface UpdateMemberActionProps {
   role: Role;
 }
 
-export async function removeMemberAction(memberId: string) {
+const inviteSchema = z.object({
+  email: z.email({ message: 'Invalid e-mail address.' }),
+  role: roleSchema,
+});
+
+export async function createInviteAction(data: FormData) {
+  const result = inviteSchema.safeParse(Object.fromEntries(data));
+
+  if (!result.success) {
+    const errors = result.error.flatten((insue) => insue.message).fieldErrors;
+
+    return { success: false, message: null, errors };
+  }
+
+  const { email, role } = result.data;
+
+  try {
+    await createInvite({
+      org: (await getCurrentOrg())!,
+      email,
+      role,
+    });
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json();
+
+      return { success: false, message, errors: null };
+    }
+
+    return { success: false, message: 'unexpected error.', errors: null };
+  }
+
+  return {
+    success: true,
+    message: 'Successfully invited the user.',
+    errors: null,
+  };
+}
+
+export async function revokeInviteAction(inviteId: string) {
   const currentOrg = await getCurrentOrg();
 
-  await removeMember({
+  await revokeInvite({
     org: currentOrg!,
-    memberId,
+    inviteId,
   });
 }
 
@@ -44,11 +86,11 @@ export async function transferOwnershipAction(transferToUserId: string) {
   });
 }
 
-export async function revokeInviteAction(inviteId: string) {
+export async function removeMemberAction(memberId: string) {
   const currentOrg = await getCurrentOrg();
 
-  await revokeInvite({
+  await removeMember({
     org: currentOrg!,
-    inviteId,
+    memberId,
   });
 }
